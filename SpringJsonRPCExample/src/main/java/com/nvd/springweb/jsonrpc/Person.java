d639658c7d7053251c0f31738f3cd4c71c2cda3c
duongbrother <duongbrother@gmail.com>
2017-01-08 11:48:01 +0700

    Cleanup

--- SpringJsonRPCExample/src/main/java/com/nvd/springweb/jsonrpc/Person.java
+++ SpringJsonRPCExample/src/main/java/com/nvd/springweb/jsonrpc/Person.java
@@ -1,6 +1,5 @@
 package com.nvd.springweb.jsonrpc;
 
-import com.google.gson.GsonBuilder;
 import com.google.gson.Gson;
 import java.util.Arrays;
 import java.util.List;
@@ -40,14 +39,5 @@ public class Person {
         sb.append(",yearOfBirth=").append(yearOfBirth);
         sb.append("}");
         return sb.toString();
-    }
-    
-    public static void main(String[] args) {
-        Person p = new Person("Solar", 212);
-        Person p2 = new Person("City", 123);
-        Gson gson = new Gson();
-        System.out.println("com.nvd.springweb.jsonrpc.Person.main()" + gson.toJson(p));
-        List<Person> persons = Arrays.asList(p, p2);
-        System.out.println("com.nvd.springweb.jsonrpc.Person.main()" + gson.toJson(persons));
     }
 }
